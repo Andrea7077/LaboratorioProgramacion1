45dc201a8196bf385afef43d4c46d8add9fe9ae4
Andrea Martinez <andreamartinezisj28@gmail.com>
2025-05-23 15:37:59 -0600

    examen

--- src/ExamenParcial1/Martienz_Andrea_Exam1.java
+++ src/ExamenParcial1/Martienz_Andrea_Exam1.java
@@ -57,93 +57,111 @@ public class Martienz_Andrea_Exam1 {
                 case 3:
                     System.out.println("\n== Piedra Papel o Tijera ==");
                     System.out.println("Bienvenido al juego de piedra, papel o tijera!");
-                    String sino="";
+                    String sino = "";
                     boolean jugar = true;
-                    
-                    String opcionjuego="";
-                    
-                    while(jugar){
-                    while (true){
-                    System.out.print("\nIngrese su eleccion entre piedra, papel o tijera: ");
-                     opcionjuego = texto.next().toLowerCase();
-                    
-                    
-                    if (opcionjuego.equals("piedra") || opcionjuego.equals("papel") || opcionjuego.equals("tijera")) {
-                        
-                        break;
-                    }else {
-                    
-                    System.out.print("\nNo ingreso ninguna de las tres opciones.");
-                    }}
-                    
-                    System.out.println("\nTu elegiste: " +opcionjuego);
-                    int computadoraeleccion = random.nextInt(3)+1; 
-                    
-                    if(computadoraeleccion == 1){
-                     System.out.println("Eleccion de la computadora es: Piedra");
-                    }else if(computadoraeleccion == 2){
-                     System.out.println("Eleccion de la computadora es: Papel");
-                    }else if (computadoraeleccion == 3){
-                     System.out.println("Eleccion de la computadora es: Tijera");
-                    }
-                    
-                   
-                    if (computadoraeleccion == 1 && opcionjuego.equals("tijera")){
-                        System.out.println("\n == La computadora gano! =="); 
-                    }if (computadoraeleccion == 1 && opcionjuego.equals("papel")){
-                        System.out.println("\n == Tu ganaste! =="); 
-                    }if (computadoraeleccion == 1 && opcionjuego.equals("piedra")){
-                        System.out.println("\n == Es un empate! =="); 
-                    }
-                    
-                     if (computadoraeleccion == 2 && opcionjuego.equals("piedra")){
-                        System.out.println("\n == La computadora gano! =="); 
-                    }if (computadoraeleccion == 2 && opcionjuego.equals("tijera")){
-                        System.out.println("\nTu ganaste!"); 
-                    }if (computadoraeleccion == 2 && opcionjuego.equals("papel")){
-                        System.out.println("\nEs un empate!"); 
-                    }
-                    
-                     if (computadoraeleccion == 3 && opcionjuego.equals("papel")){
-                        System.out.println("\n == La computadora gano! =="); 
-                    }if (computadoraeleccion == 3 && opcionjuego.equals("piedra")){
-                        System.out.println("\n == Tu ganaste! =="); 
-                    }if (computadoraeleccion == 3 && opcionjuego.equals("tijera")){
-                        System.out.println("\n == Es un empate! =="); 
-                    }
-                    
-                    System.out.print("\nDesea jugar de nuevo? (Si/No): ");
-                    sino = texto.next().toLowerCase();
-                    if(!sino.equals("si")){
+
+                    String opcionjuego = "";
+
+                    while (jugar) {
+                        while (true) {
+                            System.out.print("\nIngrese su eleccion entre piedra, papel o tijera: ");
+                            opcionjuego = texto.next().toLowerCase();
+
+                            if (opcionjuego.equals("piedra") || opcionjuego.equals("papel") || opcionjuego.equals("tijera")) {
+
+                                break;
+                            } else {
+
+                                System.out.print("\nNo ingreso ninguna de las tres opciones.");
+                            }
+                        }
+
+                        System.out.println("\nTu elegiste: " + opcionjuego);
+                        int computadoraeleccion = random.nextInt(3) + 1;
+
+                        if (computadoraeleccion == 1) {
+                            System.out.println("Eleccion de la computadora es: Piedra");
+                        } else if (computadoraeleccion == 2) {
+                            System.out.println("Eleccion de la computadora es: Papel");
+                        } else if (computadoraeleccion == 3) {
+                            System.out.println("Eleccion de la computadora es: Tijera");
+                        }
+
+                        if (computadoraeleccion == 1 && opcionjuego.equals("tijera")) {
+                            System.out.println("\n == La computadora gano! ==");
+                        }
+                        if (computadoraeleccion == 1 && opcionjuego.equals("papel")) {
+                            System.out.println("\n == Tu ganaste! ==");
+                        }
+                        if (computadoraeleccion == 1 && opcionjuego.equals("piedra")) {
+                            System.out.println("\n == Es un empate! ==");
+                        }
+
+                        if (computadoraeleccion == 2 && opcionjuego.equals("piedra")) {
+                            System.out.println("\n == La computadora gano! ==");
+                        }
+                        if (computadoraeleccion == 2 && opcionjuego.equals("tijera")) {
+                            System.out.println("\nTu ganaste!");
+                        }
+                        if (computadoraeleccion == 2 && opcionjuego.equals("papel")) {
+                            System.out.println("\nEs un empate!");
+                        }
+
+                        if (computadoraeleccion == 3 && opcionjuego.equals("papel")) {
+                            System.out.println("\n == La computadora gano! ==");
+                        }
+                        if (computadoraeleccion == 3 && opcionjuego.equals("piedra")) {
+                            System.out.println("\n == Tu ganaste! ==");
+                        }
+                        if (computadoraeleccion == 3 && opcionjuego.equals("tijera")) {
+                            System.out.println("\n == Es un empate! ==");
+                        }
+
+                        System.out.print("\nDesea jugar de nuevo? (Si/No): ");
+                        sino = texto.next().toLowerCase();
+                        if (!sino.equals("si")) {
                             jugar = false;
-                           }   
+                        }
                     }
                     break;
                 case 4:
+
+                    boolean adivinar = true;
                     System.out.println("\n== Adivinar ==");
-                    int intentosmax = 10, intento=0;
-                    int numerorandom=random.nextInt(100)+1;
+                    int intentosmax = 10,
+                     intento = 1;
+                    int numerorandom = random.nextInt(100) + 1;
                     System.out.println("Intenta adivinar el numero entre el 1 y 100! (Tienes 10 intentos)");
-                    
-                    while(intento<intentosmax){
-                        
-                        System.out.print("Intento: "+intento+1);
-                        intento++;
-                        int numerocomparar = texto.nextInt();
-                    
-                        if(numerorandom>numerocomparar){
-                            System.out.println("El numero a adivinar es mayor que "+numerocomparar);
-                            System.out.println("Intentos: "+intento+ "/10" );
+
+                    while (adivinar==true) {
+                        while (intento < intentosmax) {
+                            System.out.println("\nIntento: " + intento + "/10");
+                            System.out.print("Ingrese el que piensas que es el numero: ");
+
+                            intento++;
+                            int numerocomparar = texto.nextInt();
+
+                            if (numerorandom > numerocomparar) {
+                                System.out.println("El numero a adivinar es mayor que " + numerocomparar);
+                            } else if (numerorandom < numerocomparar) {
+                                System.out.println("El numero a adivinar es menor que " + numerocomparar);
+
+                            }
+
+                            if (numerocomparar == numerorandom) {
+                                 System.out.println("Ese era el numero, acertaste! Felicidades!");
+                                System.out.println("Adivinaste el numero en "+intento+" intentos");
+                                adivinar = false;
+                            }
+                            if(intento >= intentosmax){
+                                System.out.println("Oh no! Parece que tus intentos se terminaron");
+                                System.out.println("suerte a la proxima.");
+                            }
                         }
-                        
-                        
-                        if (numerocomparar == numerorandom){
-                            System.out.println("Ese era el numero, acertaste! Felicidades!");
-                        }
-                        
+                               
+
                     }
-                    
-                    
+
                     break;
                 case 5:
                     System.out.println("Saliendo...");
@@ -157,5 +175,5 @@ public class Martienz_Andrea_Exam1 {
             texto.close();
         }
 
-        }        
+    }
 }
