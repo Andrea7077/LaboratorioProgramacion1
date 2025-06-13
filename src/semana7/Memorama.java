e0c15487ae5868dc2e8ed6b61098dc646c6dd153
Andrea Martinez <andreamartinezisj28@gmail.com>
2025-06-13 16:56:28 -0600

    comit

--- src/semana7/Memorama.java
+++ src/semana7/Memorama.java
@@ -8,12 +8,12 @@ package semana7;
  *
  * @author andre
  */
-public class practica extends javax.swing.JFrame {
+public class Memorama extends javax.swing.JFrame {
 
     /**
      * Creates new form ejerciciosemana7
      */
-    public practica() {
+    public Memorama() {
         initComponents();
     }
 
@@ -59,21 +59,27 @@ public class practica extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(practica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Memorama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(practica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Memorama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(practica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Memorama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(practica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Memorama.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new practica().setVisible(true);
+                new Memorama().setVisible(true);
             }
         });
     }
